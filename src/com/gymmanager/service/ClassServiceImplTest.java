ae8dd25700cdb36c6d39468793f5ddf12d56592e
wfelow <weiler.felix04@gmail.com>
2025-06-24 15:48:39 +0200

    Tests (leer)

---
+++ src/com/gymmanager/service/ClassServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.gymmanager.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClassServiceImplTest {
+
+    @Test
+    void getAllClasses() {
+    }
+
+    @Test
+    void getClassById() {
+    }
+
+    @Test
+    void addClass() {
+    }
+
+    @Test
+    void updateClass() {
+    }
+
+    @Test
+    void removeClass() {
+    }
+
+    @Test
+    void getEnrollmentCount() {
+    }
+}
---
+++ src/com/gymmanager/service/MemberServiceImplTest.java
@@ -0,0 +1,27 @@
+package com.gymmanager.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MemberServiceImplTest {
+
+    @Test
+    void getAllMembers() {
+    }
+
+    @Test
+    void getMemberById() {
+    }
+
+    @Test
+    void addMember() {
+    }
+
+    @Test
+    void updateMember() {
+    }
+
+    @Test
+    void removeMember() {
+    }
+}
